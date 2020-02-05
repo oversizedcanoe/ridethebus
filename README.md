7f57fe453daedd9c149e177eb4b02194ee489a36
Billy Cook <cook0318@algonquinlive.com>
2020-02-04 22:08:51 -0500

    added rules and some additional styling

--- README.md
+++ README.md
@@ -20,8 +20,8 @@ Cards 7 through 12: Same as 1 through 6.
 2. CSS/Layout
 3. Logic for ties
 4. Logic for going backwards when complete
-5. Checks to make sure cards cannot be drawn multiple times
+5. <del>Checks to make sure cards cannot be drawn multiple times<del>
 6. Have card count shown on screen
-7. Have reset button which will add cards back to deck 
+7. <del>Have reset button which will add cards back to deck<del>
 8. show stats on screen: % of cards each colour, cards drawn, etc
-9. show result even if wrong, sleep for 3 seconds, then restart.
+9. <del>show result even if wrong, sleep for 3 seconds, then restart.<del>
--- scripts.js
+++ scripts.js
@@ -85,44 +85,44 @@ function playGame(color, highlow, inout){
 
 
 function redOrBlack(color){
-    redOrBlackGeneratedCard = generateRandomCard()
+    redOrBlackGeneratedCard = generateRandomCard();
     console.log("The red or black function's generated card is " + redOrBlackGeneratedCard.value + " of " + redOrBlackGeneratedCard.suit)
     if (color == "red"){
         if (redOrBlackGeneratedCard.suit == "heart" || redOrBlackGeneratedCard.suit == "diamond"){
-            win(redOrBlackGeneratedCard)
+            win(redOrBlackGeneratedCard);
         }else{
             counter+=1;
-            lose(redOrBlackGeneratedCard)
+            lose(redOrBlackGeneratedCard);
         }
     }
     if (color == "black"){
         if (redOrBlackGeneratedCard.suit == "spade" || redOrBlackGeneratedCard.suit == "club"){
-            win(redOrBlackGeneratedCard)
+            win(redOrBlackGeneratedCard);
         }else{
             counter+=1;
-            lose(redOrBlackGeneratedCard)
+            lose(redOrBlackGeneratedCard);
         }
     }
 }
 
 function higherLower(highlow){
-    higherLowerGeneratedCard = generateRandomCard()
-    console.log("The higher lower function's generated card is " + higherLowerGeneratedCard.value + " of " + higherLowerGeneratedCard.suit)
-    console.log("the old value of the red/black card is " + redOrBlackGeneratedCard.value + " of " + redOrBlackGeneratedCard.suit)
+    higherLowerGeneratedCard = generateRandomCard();
+    console.log("The higher lower function's generated card is " + higherLowerGeneratedCard.value + " of " + higherLowerGeneratedCard.suit);
+    console.log("the old value of the red/black card is " + redOrBlackGeneratedCard.value + " of " + redOrBlackGeneratedCard.suit);
     if (highlow == "higher"){
         if (higherLowerGeneratedCard.value > redOrBlackGeneratedCard.value){
-            win(higherLowerGeneratedCard)
+            win(higherLowerGeneratedCard);
         } else{
             counter+=1;
-            lose(higherLowerGeneratedCard)
+            lose(higherLowerGeneratedCard);
         }
     }
     if (highlow == "lower"){
         if (higherLowerGeneratedCard.value < redOrBlackGeneratedCard.value){
-            win(higherLowerGeneratedCard)
+            win(higherLowerGeneratedCard);
         } else{
             counter+=1;
-            lose(higherLowerGeneratedCard)
+            lose(higherLowerGeneratedCard);
         }
     }
     
@@ -131,55 +131,58 @@ function higherLower(highlow){
 function inOrOut(inout){
     let num1 = redOrBlackGeneratedCard.value;
     let num2 = higherLowerGeneratedCard.value;
-    console.log("Inside the InOrOut game, we are deciding between the values " + num1 + " and " + num2)
+    console.log("Inside the InOrOut game, we are deciding between the values " + num1 + " and " + num2);
     let nums = [num1, num2];
     nums.sort();
     inOutGeneratedCard = generateRandomCard();
-    console.log("inside of in or out game, the random card generated is " + inOutGeneratedCard.value + " of " + inOutGeneratedCard.suit)
+    console.log("inside of in or out game, the random card generated is " + inOutGeneratedCard.value + " of " + inOutGeneratedCard.suit);
     if (inout == "in"){
         if (inOutGeneratedCard.value < nums[1] && inOutGeneratedCard.value > nums[0]){
-            win(inOutGeneratedCard)
+            win(inOutGeneratedCard);
         } else{
             counter+=1;
-            lose(inOutGeneratedCard)
+            lose(inOutGeneratedCard);
         }
     }
     if (inout == "out"){
         if (inOutGeneratedCard.value < nums[0] || inOutGeneratedCard.value > nums[1]){
-            win(inOutGeneratedCard)
+            win(inOutGeneratedCard);
         } else{
             counter+=1;
-            lose(inOutGeneratedCard)
+            lose(inOutGeneratedCard);
         }   
     }
     
 }
 
 function updateAllCards(){
-    $(".card").css("background-image", "url('/pics/red_back.png')")
+    $(".card").css("background-image", "url('/pics/red_back.png')");
 
 }
 function updateCardBackground(pickedCard){
     let cardName = pickedCard.name;
-    // if (counter == 0){
-    //     $(".card").css("background-image", "url('/pics/red_back.png')")
-    // }
     if (counter == 1){
         $("#card1").css("background-image", "url('/pics/" + cardName + ".png')");
     }
     if (counter == 2){
-        $("#card2").css("background-image", "url('/pics/" + cardName + ".png')")
+        $("#card2").css("background-image", "url('/pics/" + cardName + ".png')");
     }
     if (counter == 3){
-        $("#card3").css("background-image", "url('/pics/" + cardName + ".png')")
+        $("#card3").css("background-image", "url('/pics/" + cardName + ".png')");
     }
     if (counter == 4){
-        $("#card4").css("background-image", "url('/pics/" + cardName + ".png')")
+        $("#card4").css("background-image", "url('/pics/" + cardName + ".png')");
     }
     if (counter == 5){
-        $("#card5").css("background-image", "url('/pics/" + cardName + ".png')")
+        $("#card5").css("background-image", "url('/pics/" + cardName + ".png')");
     }
     if (counter == 6){
-        $("#card6").css("background-image", "url('/pics/" + cardName + ".png')")
+        $("#card6").css("background-image", "url('/pics/" + cardName + ".png')");
     }
+}
+
+function resetDeck(){
+    cards.push.apply(cards, usedCards);
+    counter = 0;
+    updateAllCards();
 }
